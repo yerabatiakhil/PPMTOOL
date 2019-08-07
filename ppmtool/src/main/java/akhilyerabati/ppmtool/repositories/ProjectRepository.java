e53fd733c0d23dca88c5c609a24dbf86514e1c7d
Akhil Yerabati <akhil.yerabati@hcl.com>
2019-08-07 12:43:07 -0400

    Repo

---
+++ ppmtool/src/main/java/akhilyerabati/ppmtool/repositories/ProjectRepository.java
@@ -0,0 +1,11 @@
+package akhilyerabati.ppmtool.repositories;
+
+import akhilyerabati.ppmtool.domain.project;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<project, Long> {
+    @Override
+    Iterable<project> findAllById(Iterable<Long> longs);
+}
