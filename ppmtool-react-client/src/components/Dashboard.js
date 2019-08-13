1a4a202a632f1ae675c081c147009ade49b974fd
Akhil Yerabati <akhil.yerabati@hcl.com>
2019-08-13 00:28:51 -0400

    Section 3 initialised. System wasnt working. Working fine now

---
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
+class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Dashboard</h1>
+        <ProjectItem />
+      </div>
+    );
+  }
+}
+export default Dashboard;
